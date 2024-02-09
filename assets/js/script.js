609f22655e879fbedc4e37f3720aa210a0f21274
yahya0211 <yahyaagung65@gmail.com>
2024-02-09 10:28:04 +0700

    commit pertama

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function getData() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let massage = document.getElementById("massage").value;
+
+  console.log(name);
+  console.log(email);
+  console.log(phone);
+  console.log(massage);
+}
